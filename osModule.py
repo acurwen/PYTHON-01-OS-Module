b4b47e8800da125836a7770f3ac135f989cba6ea
Ayanna Curwen <yanwen2627@gmail.com>
2024-09-03 18:25:15 -0400

    Update osModule.py

--- osModule.py
+++ osModule.py
@@ -1,5 +1,4 @@
-# Create a Python script that counts the number of items (files and directories) in a specified directory(take dir as input). 
-# The script should display the total number of items (both files and directories) present in the specified directory. Bonus Modify your script to count the number of files and directories separately. Hint: use the os module
+# Create a Python script that displays the total number of items (both files and directories) present in the specified directory. 
 
 # import os module
 import os
